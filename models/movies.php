cccee769ce33a858eafb1e5a3168b5e19ba8e8db
Raisa Primerova (Раиса Примерова) <48605821+RayRedGoose@users.noreply.github.com>
2019-06-23 13:02:18 -0600

    update movie.php

--- models/movies.php
+++ models/movies.php
@@ -19,52 +19,12 @@ function movies_all($link){
 }
 
 function movie_new($link, $title, $genre, $year, $description, $photo) {
-	if (isset($_FILES['photo']['name']) && $_FILES['photo']['tmp_name'] !="")   {
-		$fileName = $_FILES['photo']['name'];
-		$fileTmpLoc = $_FILES['photo']['tmp_name'];
-		$fileType = $_FILES['photo']['type'];
-		$fileSize = $_FILES['photo']['size'];
-		$fileErrorMsg = $_FILES['photo']['error'];
-		$kaboom = explode('.', $fileName);
-		$fileExt = end($kaboom);
-
-		list($width, $height) = getimagesize($fileTmpLoc);
-
-		if ($width < 10 || $height < 10) {
-			$errors[] = 'Неправильный размер изображения';
-		}
-
-		$db_file_name = rand(100000000000, 999999999999) . '.' . $fileExt;
-
-		if ($fileSize > 10485760) {
-			$errors[] = "Изображение не может быть более 10мб";
-		} else if (!preg_match("/\.(gif|jpg|jpeg|png)$/i", $fileName)) {
-			$errors[] = "Расширение изображения может быть только gif, jpg, jpeg или png";
-			} else if ($fileErrorMsg == 1) {
-				$errors[] = "Неизыестная ошибка";
-			}
-
-		$photoFolderLocation = ROOT . "data/movies/";
-		$photoFolderLocationMin = ROOT . "data/movies/min/";
-		$photoFolderLocationCover = ROOT . "data/movies/cover/";
-		$uploadFile = $photoFolderLocation . $db_file_name;
-
-		move_uploaded_file($fileTmpLoc, $uploadFile);
-
-		$moveResult = move_uploaded_file($fileTmpLoc, $uploadFile);
-
-		if ($moveResult !=true) {
-			$errors[] = 'Изображение не загружено';
-		}
-
-		$resizedFileMin = $photoFolderLocationMin . $db_file_name;
-		$resizedFileCover = $photoFolderLocationCover . $db_file_name;
-
-		$miniPhoto = make_thumb($uploadFile, $resizedFileMin, '140', '200');
-		$coverPhoto = make_thumb($uploadFile, $resizedFileCover, '400', '600');
-
-		$photo = $db_file_name;
-
+	
+	if (if(!isset($_FILES['file_upload']) || $_FILES['file_upload']['error'] == UPLOAD_ERR_NO_FILE) {
+    		$resultError = "Error no file selected"; 
+	} else {
+		require_once 'photo.php';
+    		$photo = photoCreate();
 	}
 
 		// Запись данных в БД
@@ -103,53 +63,12 @@ function get_movie($link, $id){
 
 function movie_update($link, $title, $genre, $year, $description, $photo, $id) {
 
-	if (isset($_FILES['photo']['name']) && $_FILES['photo']['tmp_name'] !="")   {
-    $fileName = $_FILES['photo']['name'];
-    $fileTmpLoc = $_FILES['photo']['tmp_name'];
-    $fileType = $_FILES['photo']['type'];
-    $fileSize = $_FILES['photo']['size'];
-    $fileErrorMsg = $_FILES['photo']['error'];
-    $kaboom = explode('.', $fileName);
-    $fileExt = end($kaboom);
-
-    list($width, $height) = getimagesize($fileTmpLoc);
-
-    if ($width < 10 || $height < 10) {
-      $errors[] = 'Неправильный размер изображения';
-    }
-
-    $db_file_name = rand(100000000000, 999999999999) . '.' . $fileExt;
-
-    if ($fileSize > 10485760) {
-      $errors[] = "Изображение не может быть более 10мб";
-    } else if (!preg_match("/\.(gif|jpg|jpeg|png)$/i", $fileName)) {
-      $errors[] = "Расширение изображения может быть только gif, jpg, jpeg или png";
-      } else if ($fileErrorMsg == 1) {
-        $errors[] = "Неизыестная ошибка";
-      }
-
-    $photoFolderLocation = ROOT . "data/movies/";
-    $photoFolderLocationMin = ROOT . "data/movies/min/";
-    $photoFolderLocationCover = ROOT . "data/movies/cover/";
-    $uploadFile = $photoFolderLocation . $db_file_name;
-
-    move_uploaded_file($fileTmpLoc, $uploadFile);
-
-    $moveResult = move_uploaded_file($fileTmpLoc, $uploadFile);
-
-    if ($moveResult !=true) {
-      $errors[] = 'Изображение не загружено';
-    }
-
-    $resizedFileMin = $photoFolderLocationMin . $db_file_name;
-    $resizedFileCover = $photoFolderLocationCover . $db_file_name;
-
-    $miniPhoto = make_thumb($uploadFile, $resizedFileMin, '140', '200');
-    $coverPhoto = make_thumb($uploadFile, $resizedFileCover, '400', '600');
-
-    $photo = $db_file_name;
-    return $photo;
-  }
+	if (if(!isset($_FILES['file_upload']) || $_FILES['file_upload']['error'] == UPLOAD_ERR_NO_FILE) {
+    		$resultError = "Error no file selected"; 
+	} else {
+		require_once 'photo.php';
+    		$photo = photoCreate();
+	}
 
 	$query = "	UPDATE movies
 							SET title = '". mysqli_real_escape_string($link, $title) ."',
